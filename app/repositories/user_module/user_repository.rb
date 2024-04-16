ad02dc88bf2f4342caf9adb38f6e72b0b9bc7930
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-15 22:49:39 -0300

    add(repo rspec): create method added and tested

--- app/repositories/user_module/user_repository.rb
+++ app/repositories/user_module/user_repository.rb
@@ -1,7 +1,12 @@
 module UserModule
   class UserRepository
+
     def create(params)
-      User.create(params)
+      user = User.new(params)
+      return raise RuntimeError, I18n.t('errors.user_creation', errors: user.errors.full_messages) if !user.valid?
+
+      user.save
     end
+
   end
 end
--- spec/repositories/user/user_repository_spec.rb
+++ spec/repositories/user/user_repository_spec.rb
@@ -1,21 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe UserModule::UserRepository, type: :repository do
-  let(:user_repo)  do
-    UserRepository.new
+  let(:user_repo) { UserModule::UserRepository.new }
+
+  context "with valid params" do
+
+    describe "#create" do
+      it "creates a new user" do
+        params = {
+          name: "John",
+          last_name: "Doe",
+          email: "john@example.com",
+          password_digest: "password123",
+          password_digest_confirmation: "password123"
+        }
+
+        expect { user_repo.create(params) }.to change(User, :count).by(1)
+      end
+    end
   end
 
-  describe "#create" do
-    it "creates a new user with valid parameters" do
-      params = {
-        name: "John",
-        last_name: "Doe",
-        email: "john@example.com",
-        password: "password123",
-        password_confirmation: "password123"
-      }
+  context "with invalid params" do
 
-      expect { user_repo.create(params).to change(User, :count).by(1) }
+    describe "#create" do
+      it "raises a Runtime error" do
+        params = {
+          name: "J",
+          last_name: "D",
+          email: "22john.com",
+          password_digest: "password123",
+          password_digest_confirmation: "pa"
+        }
+
+        expect { user_repo.create(params) }.to raise_error(RuntimeError)
+      end
     end
   end
 end
