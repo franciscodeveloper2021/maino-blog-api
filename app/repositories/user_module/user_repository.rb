648e78bc58f12ed0096defed684836b42bf20746
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-16 03:08:51 -0300

    update methhod

--- app/repositories/user_module/user_repository.rb
+++ app/repositories/user_module/user_repository.rb
@@ -8,5 +8,17 @@ module UserModule
       user.save
       user
     end
+
+    def update(current_user_id, params)
+      user = User.find_by_id(current_user_id)
+      raise RuntimeError, I18n.t('errors.user_not_found', id: current_user_id) if user.nil?
+
+      user.attributes = params
+
+      raise RuntimeError, I18n.t('errors.user_update', errors: user.errors.full_messages) if !user.valid?
+
+      user.save
+      user
+    end
   end
 end
--- spec/repositories/user/user_repository_spec.rb
+++ spec/repositories/user/user_repository_spec.rb
@@ -2,20 +2,35 @@ require 'rails_helper'
 
 RSpec.describe UserModule::UserRepository, type: :repository do
   let(:user_repo) { UserModule::UserRepository.new }
+  let(:params) do
+    {
+      name: "John",
+      last_name: "Doe",
+      email: "john@example.com",
+      password: "password123",
+    }
+  end
 
   context "with valid params" do
-    let(:params) do
-      {
-        name: "John",
-        last_name: "Doe",
-        email: "john@example.com",
-        password: "password123",
-      }
-    end
 
     describe "#create" do
       it "creates a new user" do
         expect { user_repo.create(params) }.to change(User, :count).by(1)
+      end
+    end
+
+    describe "#update" do
+      it "updates current user" do
+        user = User.new(params)
+        user.save
+
+        updated_params = { name: "John Updated", password: "password_new" }
+        user_repo.update(user.id, updated_params)
+
+        user.reload
+
+        expect(user.name).to eq("John Updated")
+        expect(user.password).to eq("password_new")
       end
     end
   end
@@ -32,6 +47,25 @@ RSpec.describe UserModule::UserRepository, type: :repository do
         }
 
         expect { user_repo.create(params) }.to raise_error(RuntimeError)
+      end
+    end
+
+    describe "#update" do
+      before(:each) do
+        @user = User.new(params)
+        @user.save
+      end
+
+      it "raises a RuntimeError when user is not found" do
+        updated_params = { name: "John Updated", password: "password_new" }
+
+        expect { user_repo.update(-1, updated_params) }.to raise_error(RuntimeError)
+      end
+
+      it "raises a RuntimeError when user params are not valid" do
+        updated_params = { name: "J", password: "p" }
+
+        expect { user_repo.update(@user.id, updated_params) }.to raise_error(RuntimeError)
       end
     end
   end
