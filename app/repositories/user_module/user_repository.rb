a0a50e254530a45aecce50ed5c8acf0f10fd5fe9
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-15 23:53:57 -0300

    refactor(user rspec): removing return

--- app/repositories/user_module/user_repository.rb
+++ app/repositories/user_module/user_repository.rb
@@ -3,10 +3,9 @@ module UserModule
 
     def create(params)
       user = User.new(params)
-      return raise RuntimeError, I18n.t('errors.user_creation', errors: user.errors.full_messages) if !user.valid?
+      raise RuntimeError, I18n.t('errors.user_creation', errors: user.errors.full_messages) if !user.valid?
 
       user.save
     end
-
   end
 end
