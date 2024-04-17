479c307b3611975c036599a49c1d9a41268fce7f
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-17 18:36:17 -0300

    CRUD OK

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,26 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :authenticate_user, only: [:create, :update, :destroy]
 
   def index
     per_page = 3
     page = params.fetch(:page, 1).to_i
     offset = (page - 1) * per_page
-    @posts = Post.order(created_at: :desc).limit(per_page).offset(offset)
+    posts = Post.order(created_at: :desc).limit(per_page).offset(offset)
 
-    render json: @posts, status: :ok
+    render json: posts, status: :ok
   end
 
   def show
-    @post = Post.find(params[:id])
+    post = Post.find(params[:id])
 
-    render json: @post.slice(:id, :title, :body), status: :ok
+    render json: post.slice(:id, :title, :body), status: :ok
   end
 
   def create
     post = Post.new(post_params)
     raise RuntimeError if !post.valid?
+
+    return render json: { error: 'Unauthorized' }, status: :unauthorized unless post.user_id == current_user.id
 
     post.save
 
@@ -26,23 +28,53 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find_by_id(params[:id])
-    return render json: { error: 'Post not found' }, status: :not_found unless @post
+    post = Post.find_by_id(params[:id])
+    return render json: { error: 'Post not found' }, status: :not_found unless post
 
-    if @post.attributes = post_params
-      render json: @post.slice(:id, :title, :body), status: :ok
+    return render json: { error: 'Unauthorized' }, status: :unauthorized unless post.user_id == current_user.id
+
+    if post.update(post_params)
+      render json: post.slice(:id, :title, :body), status: :ok
     else
-      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    post = Post.find_by_id(params[:id])
+    return render json: { error: 'Post not found' }, status: :not_found unless post
+
+    return render json: { error: 'Unauthorized' }, status: :unauthorized unless post.user_id == current_user.id
+
+    post.destroy
+
+    render json: { success: "Destroyed"}, status: :ok
   end
 
   private
 
   def set_post
-    @post = Post.find(params[:id])
+    post = Post.find(params[:id])
   end
 
   def post_params
     params.require(:post).permit(:title, :body, :user_id)
+  end
+
+  def authenticate_user
+    token = request.headers['Authorization']&.split(' ')&.last
+    return render json: { error: 'Token missing' }, status: :unauthorized unless token
+
+    decoded_token = JWT.decode(token, 'your_secret_key', true, algorithm: 'HS256')
+    user_id = decoded_token.first['user_id']
+    @current_user = User.find_by(id: user_id)
+
+    render json: { error: 'Invalid token' }, status: :unauthorized unless @current_user
+  rescue JWT::DecodeError
+    render json: { error: 'Invalid token' }, status: :unauthorized
+  end
+
+  def current_user
+    @current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :update] do
   end
-  resources :posts, only: [:index, :show, :create, :update]
+  resources :posts, only: [:index, :show, :create, :update, :destroy]
   post '/login', to: 'users#log_in'
 end
