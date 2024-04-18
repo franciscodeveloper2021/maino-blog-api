5306b60b8922137a77fe62e9e92c37d16c3e29ed
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-18 04:02:19 -0300

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :update] do
   end
   resources :posts, only: [:index, :show, :create, :update, :destroy]
-  root '/login', to: 'users#log_in'
+  post '/login', to: 'users#log_in'
+
+  root 'posts#index'
 end
