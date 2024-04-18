219b3fcd3f526223dbd7be0d165fe2deb6e17dd3
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-04-18 03:55:24 -0300

    render

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :update] do
   end
   resources :posts, only: [:index, :show, :create, :update, :destroy]
-  post '/login', to: 'users#log_in'
+  root '/login', to: 'users#log_in'
 
-  root 'posts#index', defaults: { format: 'json' }
+  root 'users#welcome'
 end
